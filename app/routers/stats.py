83a6b11c437a1b3b2337c304a84666fc53a7f219
Misha Osipov <mishaosipv@gmail.com>
2026-02-16 16:07:00 +0300

    feat: add tag cloud endpoint

--- app/routers/stats.py
+++ app/routers/stats.py
@@ -20,3 +20,11 @@ async def get_stats(db: AsyncSession = Depends(get_session_db)):
     populars_tags = await db.execute(select(Tag.name, Tag.slug, func.count(post_tags.c.post_id).label("count")).join(post_tags, Tag.id == post_tags.c.tag_id).join(PostModel, PostModel.id == post_tags.c.post_id).where(PostModel.status == "published").group_by(Tag.id).order_by(func.count(post_tags.c.post_id).desc()).limit(10))
     
     return {"total_posts": all_posts, "total_comments": all_comments, "popular_tags":[{"name":row.name, "slug":row.slug, "count":row.count} for row in populars_tags]}
+
+
+
+@router.get("/tags/cloud/")
+async def get_tag_cload(db: AsyncSession = Depends(get_session_db)):
+    tags = await db.execute(select(Tag.name, Tag.slug, func.count(post_tags.c.post_id).label("count")).join(post_tags, Tag.id == post_tags.c.tag_id).join(PostModel, PostModel.id == post_tags.c.post_id).where(PostModel.status == "published").group_by(Tag.id).order_by(func.count(post_tags.c.post_id).desc()))
+    return {"tags":[{"name":row.name, "slug":row.slug, "count":row.count} for row in tags]}
+
